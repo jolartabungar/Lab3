79b9b9875dc5241f4a0983551fb9fb8ad523826f
jolar <jolar@172.17.144.69>
2018-09-17 11:46:22 -0400

    Added a few function calls in the main method of AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -9,25 +9,20 @@ public class AddressBook {
 
 	}
 
-	public static void addBuddy(BuddyInfo obuddyInfo) {
+	public void addBuddy(BuddyInfo obuddyInfo) {
 		if (obuddyInfo != null)
 			addressBook.add(obuddyInfo);
 	}
 	
-	public static void removeBuddy(int index) {
+	public void removeBuddy(int index) {
 		if (index >= 0 && index <= addressBook.size())
 			addressBook.remove(index);
 	}
 	
 	public static void main(String[] args) {
 		
-		BuddyInfo buddy = new BuddyInfo("Jak", "100 First St.", "911");
+		System.out.println("AddressBook");
 		
-		addressBook.add(buddy);
-		addressBook.add(buddy);
-		//addressBook.remove(0);
-		
-		System.out.println(addressBook.get(1).getName());
 		
 	}
 
